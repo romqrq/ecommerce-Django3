d6ceb5be392379f66d4030de09101742ddd56ab9
romqrq <rjaalbuquerque@gmail.com>
2020-09-28 16:45:34 +0000

    ADded home app and templates

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -44,6 +45,15 @@ INSTALLED_APPS = [
     'allauth',  # allauth
     'allauth.account',  # allauth - basic login and out
     'allauth.socialaccount',  # allauth - handles loggin via social media
+    'home',
+    
+    # 'bag',
+    # 'checkout',
+    # 'profiles',
+
+    # Other
+    # 'crispy_forms',
+    # 'storages',
 ]
 
 MIDDLEWARE = [
@@ -61,7 +71,10 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
